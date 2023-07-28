9b912559f0b1f24ca1de2a36991c302dad43014b
Seth Schwartz <sethschwartz@seths-mbp-work.lan>
2023-07-28 17:53:40 -0400

    Lifting State

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,69 @@
 import * as React from 'react'
-
-const stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'TanStack Query',
-    url: 'https://tanstack.com/query/v3/',
-    author: 'Tanner Linsley',
-    num_comments: 4,
-    points: 6,
-    objectID: 2,
-  },
-  {
-    title: 'Next.js',
-    url: 'https://nextjs.org/',
-    author: 'Dijon Musters',
-    num_comments: 8,
-    points: 3,
-    objectID: 3,
-  },
-];
+// import { useEffect } from 'react';
 
 const App = () => {
-  function handleSearch(event) {
-    console.log(event.target.value)
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'TanStack Query',
+      url: 'https://tanstack.com/query/v3/',
+      author: 'Tanner Linsley',
+      num_comments: 4,
+      points: 6,
+      objectID: 2,
+    },
+    {
+      title: 'Next.js',
+      url: 'https://nextjs.org/',
+      author: 'Dijon Musters',
+      num_comments: 8,
+      points: 3,
+      objectID: 3,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('')
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
   }
+
+  const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch}/>
+      <Search onSearch={handleSearch} />
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   )
 }
 
 
 const Search = ({onSearch}) => {
-  const [searchTerm, setSearchTerm] = React.useState('')
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value)
-
-    onSearch(event)
-  }
-
-  const handleBlur = (event) => {
-    console.log(event)
-    console.log(event.target.value)
-  }
-
   return (
     <div>
       <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' onChange={handleChange} onBlur={handleBlur} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id='search' type='text' onChange={onSearch} />
     </div>
   )
 }
