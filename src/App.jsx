8692a6f826d3e0b7c88083290e872c6d06b35c27
Seth Schwartz <sethschwartz@seths-mbp-work.lan>
2023-07-28 16:31:17 -0400

    Callback handlers

--- src/App.jsx
+++ src/App.jsx
@@ -35,24 +35,32 @@ const stories = [
   },
 ];
 
-const App = () => (
+const App = () => {
+  function handleSearch(event) {
+    console.log(event.target.value)
+  }
+
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch}/>
 
       <hr />
 
       <List list={stories} />
     </div>
-)
+  )
+}
 
 
-const Search = () => {
+const Search = ({onSearch}) => {
   const [searchTerm, setSearchTerm] = React.useState('')
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value)
+
+    onSearch(event)
   }
 
   const handleBlur = (event) => {
