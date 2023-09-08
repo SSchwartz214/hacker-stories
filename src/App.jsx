1fdf34d947a8d38a3437ad5b621a6f9a9a11f360
Seth Schwartz <sethschwartz@Seths-MBP-Work.lan>
2023-09-08 14:41:43 -0400

    Data Re-Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -62,9 +62,11 @@ const App = () => {
 
   React.useEffect(() => {
     localStorage.setItem('search', searchTerm)
+    if (!searchTerm) return
+
     dispatchStories({ type: 'STORIES_FETCH_INIT'})
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -79,8 +81,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value)
   }
-
-  const searchedStories = stories.data.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -109,7 +109,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   )
